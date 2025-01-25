6830a69a94b16aa31c7d293811cf61ebf8bb0b94
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 16:16:04 +0530

    Add files via upload

---
+++ vowelcount.java
@@ -0,0 +1,20 @@
+
+public class vowelcount{
+    public static void main(String[] args) {
+        String inputString = "Hello, World!";
+        int vowelCount = 0;
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char ch = inputString.charAt(i);
+            if (vowels.indexOf(ch) != -1) {
+                vowelCount++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+}
+
+    
+
